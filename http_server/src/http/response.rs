6960fb319c1527e36e0cd9684018f552dbf53708
Hans-J. Schmid <knock@myopendoor.de>
2023-06-26 05:29:18 +0200

    reformats source code.

--- http_server/src/http/response.rs
+++ http_server/src/http/response.rs
@@ -4,27 +4,27 @@ use super::StatusCode;
 
 #[derive(Debug)]
 pub struct Response {
-    status_code: StatusCode,
-    body: Option<String>,
+  status_code: StatusCode,
+  body: Option<String>,
 }
 
 impl Response {
-    pub fn new(status_code: StatusCode, body: Option<String>) -> Self {
-        Response { status_code, body }
-    }
+  pub fn new(status_code: StatusCode, body: Option<String>) -> Self {
+    Response { status_code, body }
+  }
 
-    pub fn send(&self, stream: &mut impl Write) -> IoResult<()> {
-        let body = match &self.body {
-            Some(b) => b,
-            None => "",
-        };
+  pub fn send(&self, stream: &mut impl Write) -> IoResult<()> {
+    let body = match &self.body {
+      Some(b) => b,
+      None => "",
+    };
 
-        write!(
-            stream,
-            "HTTP/1.1 {} {}\r\n\r\n{}",
-            self.status_code,
-            self.status_code.reason_phrase(),
-            body
-        )
-    }
+    write!(
+      stream,
+      "HTTP/1.1 {} {}\r\n\r\n{}",
+      self.status_code,
+      self.status_code.reason_phrase(),
+      body
+    )
+  }
 }
